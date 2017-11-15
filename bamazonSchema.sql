f58a0f9e28ee5fe8befa6bcedb48b81d290771a5
Denny Wu <denny.wu87@gmail.com>
2017-11-14 23:19:31 -0800

    Wrote code to add products to database

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -11,3 +11,33 @@ CREATE TABLE products(
   stock_quantity INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Dictionary", "Books", 10.99, 100)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Computer Programming Textbook, Volume 1", "Books", 11.99, 50)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Computer Programming Textbook, Volume 2", "Books", 11.99, 50)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Computer Programming Textbook, Volume 3", "Books", 11.99, 50)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Smartphone", "Electronics", 300.99, 100)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Laptop", "Electronics", 1000.99, 150)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Desktop Computer", "Electronics", 2000.99, 120)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Cat Food", "Pet Supplies", 29.99, 200)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Human Food", "Groceries", 80.99, 300)
+
+INSERT INTO products (product_name, department_name, price stock_quantity)
+VALUES ("Pens (One Box)", "Office Supplies", 6.99, 150)
