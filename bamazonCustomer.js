770c4fb867610fb3dc7faf3c09d31a5a31a97690
Denny Wu <denny.wu87@gmail.com>
2017-11-29 00:13:51 -0800

    Finished code for minimum requirement, pending review

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,6 @@ connection.connect(function(err) {
 	if (err) throw err;
 
 	displayStock();
-	purchase();
 	
 });
 
@@ -33,37 +32,70 @@ function displayStock() {
       	}
 
 	})
+
+	purchase();
+	
 }
 
 function purchase() {
-	
-	inquirer
-		.prompt([
-			{
-				name: "product_id",
-		        type: "input",
-		        message: "What is the ID of the product you want to buy?"
-			},
-			{
-				name: "product_amount",
-		        type: "input",
-		        message: "How many units of this product do you want?"
-			}
-		])
-		.then(function(answer) {
 
-			connection.query(
-				"SELECT * FROM products WHERE item_id", [answer.product_id],
-			)
+	connection.query("SELECT * FROM products", function(err, results) {
+		
+		if (err) throw err;
 
-			connection.query(
-				"UPDATE products SET ? WHERE ?",
-				[
-					{
-						// stock_quantity: stock_quantity - answer.product_amount
-					}
-				]
-			)
+		inquirer
+			.prompt([
+				{
+					name: "product_id",
+			        type: "input",
+			        message: "What is the ID of the product you want to buy?"
+				},
+				{
+					name: "product_amount",
+			        type: "input",
+			        message: "How many units of this product do you want?"
+				}
+			])
+			.then(function(answer) {
 
-		})
+				var selectedItem;
+				var selectedAmount = parseInt(answer.product_amount);
+
+				for (var i = 0; i < results.length; i++) {
+					if (results[i].item_id === answer.product_id) {
+			    		selectedItem = results[i];
+			        }
+				}
+
+				if (selectedItem.stock_quantity < selectedAmount || selectedItem.stock_quantity === 0) {
+
+					console.log("Sorry, we do not have enough of that item.")
+					displayStock();
+
+				} else {
+
+					var purchaseID = selectedItem.item_id
+					var newAmount = selectedItem.stock_quantity - selectedAmount
+
+					connection.query(
+			            "UPDATE purchase SET ? WHERE ?",
+			            [
+			              {
+			                stock_quantity: newAmount
+			              },
+			              {
+			                item_id: purchaseID
+			              }
+			            ],
+			            function(error) {
+			              if (error) throw err;
+
+			              console.log("Thank you for your purchase!");
+			              displayStock();
+			            }
+			        );
+
+				}
+			})
+	})	
 }
